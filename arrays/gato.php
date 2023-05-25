08e4aae7d07a6c1257baa23a1428a25173b0589b
aeeew <gabriel.costa09.gc@gmail.com>
2023-05-25 20:53:38 -0300

    Criando gato.php
    array gato.php

---
+++ arrays/gato.php
@@ -0,0 +1,29 @@
+<?php
+
+$gato = [   
+    [   
+        "foto" => "img-gato/racao1.png",     
+        "nome" => "Ração Seca para Gatos Adultos Castrados",
+        "peso" => "10kg",
+        "valor" => 176.90,        
+        
+    ],
+    [       
+        "foto" => "img-gato/areiasanitaria.png",     
+        "nome" => "Areia Sanitária Kelco Pipicat Multicat",
+        "peso" => "12kg",
+        "valor" => 41.90,  
+    ],
+    [    "foto" => "img-gato/granulado.png",     
+        "nome" => "Granulado Higiênico de Madeira CarePet",
+        "peso" => "10kg",
+        "valor" => 62.99,    
+    ],
+    [  
+        "foto" => "img-gato/racao2.png",     
+        "nome" => "Ração Royal Canin para Gatos Adultos Castrados",
+        "peso" => "10kg",
+        "valor" => 303.59, 
+    ],
+
+];
