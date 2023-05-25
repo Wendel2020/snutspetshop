f2eac96098cfc0f8ec5412b994a9ce06b38e3d36
aeeew <gabriel.costa09.gc@gmail.com>
2023-05-25 20:51:35 -0300

    Criando cachorro.php
    array cachorro.php

---
+++ arrays/cachorro.php
@@ -0,0 +1,29 @@
+<?php
+
+$cachorro = [   
+    [   
+        "foto" => "img-cachorro/racao1.png",     
+        "nome" => "Nutrilos Pro+ - Frango e Carne",
+        "peso" => "20kg",
+        "valor" => 175.41,        
+        
+    ],
+    [       
+        "foto" => "img-cachorro/racao2.png",     
+        "nome" => "Ração Seca para Cães Adultos",
+        "peso" => "7kg",
+        "valor" => 272.90,  
+    ],
+    [    "foto" => "img-cachorro/racao3.png",     
+        "nome" => "Ração Premier Golden Formula - Cães Filhotes",
+        "peso" => "20kg",
+        "valor" => 222.90,    
+    ],
+    [  
+        "foto" => "img-cachorro/biscoito.png",     
+        "nome" => "Biscoito Premier Cookie ",
+        "peso" => "250g",
+        "valor" => 16.40, 
+    ],
+
+];
