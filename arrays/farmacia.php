15a540b3f701c8b7ae3a23e773d79b63139dc513
aeeew <gabriel.costa09.gc@gmail.com>
2023-05-25 20:52:10 -0300

    Criando farmacia.php
    array farmacia.php

---
+++ arrays/farmacia.php
@@ -0,0 +1,29 @@
+<?php
+
+$farmacia = [   
+    [   
+        "foto" => "img-farmacia/gatocarrapato.png",     
+        "nome" => "Revolution 6% para Gatos de 2,5 a 7,5 kg",
+        "tamanho" => "45 mg",
+        "valor" => 208.50,        
+        
+    ],
+    [       
+        "foto" => "img-farmacia/dogcarrapato.png",     
+        "nome" => "Antipulgas e Carrapato para Cães de 4 a 10 Kg",
+        "tamanho" => "28,3 mg",
+        "valor" => 85.90,  
+    ],
+    [    "foto" => "img-farmacia/supcat.png",     
+        "nome" => "Suplemento Alimentar Agener Pasta Oral",
+        "tamanho" => "70 g",
+        "valor" => 7.85,    
+    ],
+    [  
+        "foto" => "img-farmacia/vermedog.png",     
+        "nome" => "Vermífugo Virbac Endogard para Cães até 10 Kg",
+        "tamanho" => "6 comprimidos",
+        "valor" => 16.00, 
+    ],
+
+]; ?>
