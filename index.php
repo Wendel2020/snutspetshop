afeb0fac9f376d146a8772ddce9400df1aeb1111
Wendel Neres <vanusabrumado@gmail.com>
2023-05-25 21:41:50 -0300

    criando index.php
    Criando o index da pagina

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+   
+    <title>Snuts - O melhor petshop para seu pet</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <img class="logo-cabecalho" src="imagens/logo.png" alt="snuts-petshop" >
+            <nav class= "menu-navegacao">
+               <div class="imagens"> 
+                <img class="imagem1-menu-navegacao" src="imagens/osso1.png" >
+                <img class="imagem2-menu-navegacao" src ="imagens/osso2.png">
+                <img class="imagem2-menu-navegacao"src ="imagens/osso3.png">
+                <img class="imagem3-menu-navegacao"src ="imagens/osso4.png">
+               </div>
+               <div class="texto-navegacao">
+                    <a class="menu-navegacao-item">INÍCIO</a>
+                    <a class="menu-navegacao-item">CATEGORIAS</a>
+                    <a class="menu-navegacao-item">SERVIÇOS</a>
+                    <a class="menu-navegacao-item">CARRINHO</a>
+                    <a class="menu-navegacao-item">LOGIN</a>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main class="conteudo">
+    <section class="conteudo-esquerda">
+        <div >
+            <img class="banner1" src="imagens/banner.png" alt="">
+        </div>
+        <div class="opcoes">
+            <div class="opcao-cachorro">
+                <h3 class="titulo-opcao-cachorro">CÃES</h3>
+                <a href="opcao-caes.php"><img class="imagem-opcao-cachorro" src="imagens/cachorro.png"></a>
+            </div>
+            <div class="opcao-gato">
+                <h3 class="titulo-opcao-gato">GATOS</h3>
+                <a href="opcao-gato.php"><img class="imagem-opcao-gato" src="imagens/gato.png"></a>
+            </div>
+                <div class="opcao-brinquedos">
+                <h3 class="titulo-opcao-brinquedos">BRINQUEDOS</h3>
+                <a href="opcao-brinquedo.php"><img class="imagem-opcao-brinquedos" src="imagens/brinquedos.png"></a>
+            </div>
+        </div>
+        <div class="opcoes2">
+            <div class="opcao-higiene">
+                <h3 class="titulo-opcao-higiene">HIGIENE</h3>
+                <a href="opcao-higiene.php"><img class="imagem-opcao-higiene" src="imagens/higiene.png"></a>
+            </div>
+            <div class="opcao-farmacia">
+                <h3 class="titulo-opcao-farmacia">FARMÁCIA</h3>
+                <a href="opcao-farmacia.php"><img class="imagem-opcao-farmacia" src="imagens/farmacia.png"></a>
+            </div>
+
+        </div>
+    </section>
+    <section class="conteudo-direita">
+        <img class="banner2" src="imagens/banner2.png" alt="">
+
+    </section>
+
+    </main>
+    <footer class="rodape">
+        <div class="redes-sociais">
+            <h4>REDES SOCIAS</h4>
+            <img src="imagens/instagram.png" alt="">
+            <img src="imagens/facebook.png" alt="">
+            <img src="imagens/youtube.png" alt="">
+            <img src="imagens/tiktok.png" alt="">
+
+        </div>
+        <div class="contatos">
+            <h4>CONTATOS</h4>
+            <p><img src ="imagens/telefone.png">(77) 3441-0000</p>
+            <p><img src ="imagens/email.png"> 000000@gmail.com</p>
+            <p><img src ="imagens/zap.png">(77) 3441-0000</p>
+        </div>
+        <div class="formas de pagamentos">
+            <h4>FORMAS DE PAGAMENTO</h4>
+            <img src="imagens/formasdepagamento.png" alt="">
+        </div>
+
+    </footer>
+    
+</body>
+</html>
