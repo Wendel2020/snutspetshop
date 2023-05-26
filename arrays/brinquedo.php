f391f90d78d30113a29047204d91eef87be4fa1f
Wendel Neres <vanusabrumado@gmail.com>
2023-05-25 21:07:51 -0300

    criando higiene.php
    criando o array higiene.php

---
+++ arrays/brinquedo.php
@@ -0,0 +1,28 @@
+<?php
+$brinquedo = [   
+    [   
+        "foto" => "img-brinquedo/brinquedo1.png",     
+        "nome" => "Brinquedo Furacão Pet Interativo Gatorama",
+        "tamanho" => "Unico",
+        "valor" => 40.49,        
+        
+    ],
+    [       
+        "foto" => "img-brinquedo/brinquedo2.png",     
+        "nome" => "Brinquedo LCM Bola de Tênis Mini Amarela",
+        "tamanho" => "Unico",
+        "valor" => 10.20,  
+    ],
+    [    "foto" => "img-brinquedo/brinquedo3.png",     
+        "nome" => "Brinquedo Mordedor Chalesco Rope Ball Plus",
+        "tamanho" => "Unico",
+        "valor" => 12.70,    
+    ],
+    [  
+        "foto" => "img-brinquedo/brinquedo4.png",     
+        "nome" => "Bola Macaquinho com Dispenser de Ração",
+        "tamanho" => "M",
+        "valor" => 38.60, 
+    ],
+
+];
---
+++ arrays/higiene.php
@@ -0,0 +1,29 @@
+<?php
+$higiene = [   
+    [   
+        "foto" => "img-higiene/shampooyork.png",     
+        "nome" => "Shampoo Sanol Dog Antipulgas para Cães",
+        "tamanho" => "500 ml",
+        "valor" => 15.0,        
+        
+    ],
+    [       
+        "foto" => "img-higiene/sabonetecg.png",     
+        "nome" => "Sabonete Cão/gato Neutro 90g Sanol",
+        "tamanho" => "90 g",
+        "valor" => 7.70,  
+    ],
+    [    "foto" => "img-higiene/tapete3.png",     
+        "nome" => "Tapete Higiênico Confort Pads",
+        "tamanho" => " 30 Unidades",
+        "valor" => 42.40,    
+    ],
+    [  
+        "foto" => "img-higiene/pipicat.png",     
+        "nome" => "Pipicat - Areia Higiênica Multicat para Gatos",
+        "tamanho" => "12kg",
+        "valor" => 39.90, 
+    ],
+
+];
+
